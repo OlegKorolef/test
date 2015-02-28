c35e57bc4ae8495c34b1bd46e454d2df76ae53ca
OlegKorolef <gi1vrosr@mail.ru>
2015-02-28 21:00:30 +0400

    Дописал потерянный File.php в третье ДЗ.

---
+++ Korolev_ExercisePHP2_3/classes/File1.php
@@ -0,0 +1,26 @@
+<?php
+
+class File
+{
+    public function file_upload($field)
+    {
+        if (empty($_FILES))
+            return false;
+        if (0 !== $_FILES[$field]['error'])
+            return false;
+        if ('text/html' !== $_FILES[$field]['type'])
+            return false;
+        if (is_uploaded_file($_FILES[$field]['tmp_name'])) {
+            $res =  move_uploaded_file(
+                $_FILES[$field]['tmp_name'],
+                __DIR__ . '/../news/' . $_FILES[$field]['name']
+            );
+            if (!$res) {
+                return false;
+            } else {
+                return '/news/' . $_FILES[$field]['name'];
+            }
+        }
+        return false;
+    }
+}

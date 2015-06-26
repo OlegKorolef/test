f0991ef952250921ce5ae507b4bc081daa2d51d0
OlegKorolef <gi1vrosr@mail.ru>
2015-06-26 20:18:57 +0400

    Создание KoroleffPortfolio

--- Korolev_ExercisePHP2_6/classes/E404_E403_Exception.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class E404_E403_Exception
-    extends Exception
-{
-
-}
--- Korolev_ExercisePHP2_7/classes1/E404_E403_Exception.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Applications\Classes1;
-
-class E404_E403_Exception
-    extends \Exception
-{
-
-}
